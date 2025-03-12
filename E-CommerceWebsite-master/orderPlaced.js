d0acebb0090948f365f9a08f49d5d72f74c4e6d4
veena694 <119649485+veena694@users.noreply.github.com>
2025-03-12 23:54:03 +0530

    Update orderPlaced.js

--- E-CommerceWebsite-master/orderPlaced.js
+++ E-CommerceWebsite-master/orderPlaced.js
@@ -10,15 +10,12 @@ httpRequest.onreadystatechange = function()
 {
     if(httpRequest.readyState == 4 && httpRequest.status == 200)
     {
-        // convert JSON into JavaScript object
         jsonArray = JSON.parse(httpRequest.responseText)
         console.log(jsonArray)    
         jsonArray.push(
             {
                 "id": (jsonArray.length)+1, "amount": 200,"product":["userOrder"]
             })
-
-        // send with new request the updated JSON file to the server:
         httpRequest.open("POST", jsonRequestURL, true)
         httpRequest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
         httpRequest.send(jsonArray)
